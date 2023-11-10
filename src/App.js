9f09a12aeb3c62162ad3fd337c6965a9987b4564
ACrowning <holovan24@gmail.com>
2023-11-10 15:07:55 +0200

    Finnaly

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from "react";
 import List from "./e-commerce/list";
 import MockProducts from "./e-commerce/mock";
-import AddNew from "./e-commerce/addList";
 
 function App() {
   return (
     <div>
-      <div>
-        <AddNew />
-      </div>
       <List products={MockProducts} />
     </div>
   );
