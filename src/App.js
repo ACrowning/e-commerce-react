3728d9510e44403b532bef3acc3bd99f25b04921
ACrowning <holovan24@gmail.com>
2023-11-12 15:53:54 +0200

    Can't fusing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,61 @@
-import React from "react";
+import { useState } from "react";
 import List from "./e-commerce/list";
 import MockProducts from "./e-commerce/mock";
 
 function App() {
+  const [products, setProducts] = useState(MockProducts);
+
+  const addItem = () => {
+    setProducts([...products, newProduct]);
+  };
+
+  const newProduct = {
+    id: `${products.length + 1}`,
+    title: 1,
+    description: 1,
+    price: 1,
+    favorite: false,
+    count: 1,
+  };
+
   return (
     <div>
-      <List products={MockProducts} />
+      <div>
+        <div>
+          <input
+            className="inputTitle"
+            type="text"
+            placeholder="Enter the title"
+          />
+        </div>
+        <div>
+          <input
+            className="inputDescription"
+            type="text"
+            placeholder="Enter the description"
+          />
+        </div>
+        <div>
+          <input
+            className="inputPrice"
+            type="number"
+            placeholder="Enter the price"
+          />
+        </div>
+        <div>
+          <input
+            className="inputCount"
+            type="number"
+            placeholder="Enter the count"
+          />
+        </div>
+      </div>
+
+      <button onClick={addItem}>Add Item</button>
+
+      <div>
+        <List products={MockProducts} />
+      </div>
     </div>
   );
 }
