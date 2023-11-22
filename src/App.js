f10c2b2fde405417d7f2ccfda6f1f88ee76d961c
ACrowning <holovan24@gmail.com>
2023-11-22 09:47:46 +0200

    Refactoring

--- src/App.js
+++ src/App.js
@@ -1,92 +1,18 @@
-import { useState } from "react";
-import Items from "./e-commerce/items";
-import MockProducts from "./e-commerce/mock";
-import styles from "./e-commerce/styles.module.css";
+import React from "react";
+import Navbar from "./components/navbar/navbar";
+import Products from "./components/products";
+import styles from "./components/styles.module.css";
 
 function App() {
-  const [products, setProducts] = useState(MockProducts);
-  const [inputTitle, setInputTitle] = useState("");
-  const [inputDescription, setInputDescription] = useState("");
-  const [inputPrice, setInputPrice] = useState("");
-  const [inputCount, setInputCount] = useState("");
-
-  const addItem = () => {
-    const newProduct = {
-      id: `${products.length + 1}`,
-      title: inputTitle,
-      description: inputDescription,
-      price: inputPrice,
-      favorite: false,
-      count: inputCount,
-    };
-
-    setProducts([...products, newProduct]);
-    setInputTitle("");
-    setInputDescription("");
-    setInputPrice("");
-    setInputCount("");
-  };
-
-  const deleteProduct = (itemsIndex) => {
-    const updatedProducts = [
-      ...products.slice(0, itemsIndex),
-      ...products.slice(itemsIndex + 1),
-    ];
-    setProducts(updatedProducts);
-  };
-
   return (
     <div className={styles.containerStyle}>
       {/* Вынеси этот navbar в папку components. Все стили, связанные с этим компонентом, должны быть вынесены в отдельный файл */}
       {/* Тут ты столкнёшься с проблемой: ты не сможешь добавить новый айтем в продукты, потому что все методы будут в новом компоненте */}
       {/* Напиши мне, когда дойдёшь до этого */}
-      <div className={styles.navbar}>
-        <div>
-          <input
-            value={inputTitle}
-            onChange={(event) => setInputTitle(event.target.value)}
-            type="text"
-            placeholder="Enter the title"
-          />
-        </div>
-        <div>
-          <input
-            value={inputDescription}
-            onChange={(event) => setInputDescription(event.target.value)}
-            type="text"
-            placeholder="Enter the description"
-          />
-        </div>
-        <div>
-          <input
-            value={inputPrice}
-            onChange={(event) => setInputPrice(event.target.value)}
-            type="number"
-            placeholder="Enter the price"
-          />
-        </div>
-        <div>
-          <input
-            value={inputCount}
-            onChange={(event) => setInputCount(event.target.value)}
-            type="number"
-            placeholder="Enter the count"
-          />
-        </div>
-        <button onClick={addItem}>Add</button>
-      </div>
-
+      <Navbar />
       <div className={styles.divProducts}>
         <ul className={styles.ulStyle}>
-          {/* Создай отдельный компонент Products, в котором будет лежать код с продуктами ниже */}
-          {products.map((product, productIndex) => (
-            <div className={styles.productStyle} key={product.id}>
-              <Items product={product} />
-              <button onClick={() => deleteProduct(productIndex)}>
-                Delete
-              </button>
-            </div>
-          ))}
+          <Products />
         </ul>
       </div>
     </div>
---
+++ src/components/product.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Product({ product }) {
+  return (
+    <li>
+      <img src="https://picsum.photos/200/300?random" alt={product.title} />
+      <div>
+        {product.title}, {product.description}, {product.price}
+      </div>
+    </li>
+  );
+}
